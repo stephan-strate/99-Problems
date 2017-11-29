6993362a0a096835726d8f2dd314e80e8066a71c
Stephan Strate <development@famstrate.com>
2017-11-29 18:28:04 +0100

    solved more problems

--- One.hs
+++ One.hs
@@ -18,3 +18,21 @@ elementAt :: [a] -> Int -> a
 elementAt (x : _) 1             = x
 elementAt _ n | n <= 0          = error "Negative index."
 elementAt (_ : xs) n | n > 0    = elementAt xs (n - 1)
+
+-- Problem 4
+myLength :: [a] -> Int
+myLength = foldl (\x _ -> x + 1) 0
+
+-- Problem 5
+myReverse :: [a] -> [a]
+myReverse = foldl (\x xs -> xs : x) []
+
+-- Problem 6
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome []     = True
+isPalindrome [_]    = True
+isPalindrome xs     = (isPalindrome (init (tail xs))) && ((head xs) == (myLast xs))
+
+-- Problem 7
+data NestedList a = Elem a | List [NestedList a]
+flatten :: NestedList a -> [a]
