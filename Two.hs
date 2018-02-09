a6ed98c43a9d5aff5a1d7a27bda5ff144cf1c8f4
Stephan Strate <development@famstrate.com>
2018-02-09 19:25:54 +0100

    added problem 14 to 16

--- Two.hs
+++ Two.hs
@@ -1,1 +1,20 @@
 module Problems.Two where
+
+-- Problem 11
+
+-- Problem 14
+dupli :: Eq a => [a] -> [a]
+dupli = foldl (\xs x -> if (x `elem` xs) then xs else xs ++ [x]) []
+
+-- Problem 15
+repli :: [a] -> Int -> [a]
+repli ls n = foldl (\xs x -> xs ++ handle x) [] ls
+    where
+        handle x = map (\_ -> x) [1..n]
+
+-- Problem 16
+dropEvery :: [a] -> Int -> [a]
+dropEvery ls n = loop ls n n
+    where
+        loop (_ : xs) n 0 = loop xs n n
+        loop (x : xs) n curr = x : loop xs n (curr - 1)
