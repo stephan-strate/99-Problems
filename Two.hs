e07dc6d0356195397c7969f21c8747dc26e196f7
Stephan Strate <development@famstrate.com>
2018-02-15 13:27:35 +0100

    added problem 17 and 18

--- Two.hs
+++ Two.hs
@@ -1,7 +1,5 @@
 module Problems.Two where
 
--- Problem 11
-
 -- Problem 14
 dupli :: Eq a => [a] -> [a]
 dupli = foldl (\xs x -> if (x `elem` xs) then xs else xs ++ [x]) []
@@ -18,3 +16,16 @@ dropEvery ls n = loop ls n n
     where
         loop (_ : xs) n 0 = loop xs n n
         loop (x : xs) n curr = x : loop xs n (curr - 1)
+
+-- Problem 17
+split :: String -> Int -> (String, String)
+split str n = (a, b)
+    where
+        a = take n str
+        b = drop n str
+
+-- Problem 18
+slice :: String -> Int -> Int -> String
+slice _ _ 0         = []
+slice (x:xs) 1 r    = x : slice xs 1 (r - 1)
+slice (x:xs) l r    = slice xs (l - 1) (r - 1)
